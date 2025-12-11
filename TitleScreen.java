e8074373b0722802f7fb86ea2c8926b5cab4b40a
kolbyng <440021056@gapps.yrdsb.ca>
2025-12-11 14:03:08 -0500

    title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -2,9 +2,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class TitleScreen extends World
 {
+    Label title = new Label("Red Light, Green Light", 50);
     public TitleScreen()
     {    
         super(600, 400, 1); 
-        
+        addObject(title, 300, 200);
+        Label instructions = new Label("Press 'space' to start", 24);
+        addObject(instructions, 300, 250);
+    }
+    
+    public void act()
+    {
+        if (Greenfoot.isKeyDown("space"))
+        {
+            Greenfoot.setWorld(new GameWorld());
+        }
     }
 }
