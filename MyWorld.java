e1be65a6aeef992690c8d75732b29c307b9df53f
kolbyng <440021056@gapps.yrdsb.ca>
2025-12-13 12:20:09 -0500

    removed more

--- MyWorld.java
+++ MyWorld.java
@@ -4,10 +4,5 @@ import lib.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
-        // Load the doll model and display it using ModelViewer
-        ModelData data = FileLoader.readObjFile("doll_EXPORT.obj");
-        ModelViewer viewer = new ModelViewer(data, getWidth(), getHeight(), 300, Color.BLACK);
-        viewer.setControllable(true);
-        addObject(viewer, getWidth() / 2, getHeight() / 2);
     }
 }
