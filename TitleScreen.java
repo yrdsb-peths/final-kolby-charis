45370312dd99e00e36e5b9d5f6fbeb740e9517a5
kolbyng <440021056@gapps.yrdsb.ca>
2025-12-16 14:26:06 -0500

    added background and aligned text

--- TitleScreen.java
+++ TitleScreen.java
@@ -2,13 +2,17 @@ import greenfoot.*;
 
 public class TitleScreen extends World
 {
-    Label title = new Label("Red Light, Green Light", 50);
+    Label title = new Label("Red Light, Green Light", 60);
     public TitleScreen()
     {    
         super(600, 400, 1); 
-        addObject(title, 300, 200);
-        Label instructions = new Label("Press 'space' to start", 24);
-        addObject(instructions, 300, 250);
+        GreenfootImage background = new GreenfootImage("light.jpg");
+        background.scale(600, 400);
+        setBackground(background);
+
+        addObject(title, 300, 150);
+        Label instructions = new Label("Press 'space' to start", 40);
+        addObject(instructions, 300, 225);
     }
 
     public void act()
