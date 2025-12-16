844e076756967085f5cd287747899352a4057a45
kolbyng <440021056@gapps.yrdsb.ca>
2025-12-16 17:18:24 -0500

    added game background

--- GameWorld.java
+++ GameWorld.java
@@ -5,7 +5,7 @@ public class GameWorld extends World
     public GameWorld()
     {    
         super(600, 400, 1); 
-        GreenfootImage background = new GreenfootImage("background.png");
+        GreenfootImage background = new GreenfootImage("game setting.png");
         background.scale(600, 400);
         setBackground(background);
     }
