564a373a2d19344d019d57c5fcfd3f3a4ef9c5d5
charis18 <charis.tyc0108@gmail.com>
2025-12-17 09:12:22 -0500

    Add Doll turning

--- Doll.java
+++ Doll.java
@@ -2,14 +2,24 @@ import greenfoot.*;
 
 public class Doll extends Actor
 {
+    GreenfootImage front = new GreenfootImage("front.png");
+    front.scale(50, 75);
+
+    GreenfootImage back = new GreenfootImage("back.png");
+    back.scale(50, 75);
+    
     public Doll()
     {
-        GreenfootImage front = new GreenfootImage("front.png");
-        front.scale(50, 75);
-
-        GreenfootImage back = new GreenfootImage("back.png");
-        back.scale(50, 75);
-
         setImage(front);
+    }
+
+    public void faceFront()
+    {
+        setImage(front);
+    }
+
+    public void faceBack()
+    {
+        setImage(back);
     }
 }
